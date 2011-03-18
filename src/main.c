29277b5b54425d8ccc5d04447bab261f9efbb20f
Christoph Lang <mail.clang@gmail.com>
2011-03-18 03:18:58 +0100

    added xcode 4 project

--- src/main.c
+++ src/main.c
@@ -8,9 +8,10 @@
 
 int main(int32_t argc, const char** argv) {
   mveLog(MVE_LOG_INFO, "starting up");
+  glfwInit();
+  
   MVEconfig* config = mveInitConfig(MVE_CONFIG_FILE_NAME);
 
-  glfwInit();
   glfwOpenWindowHint(GLFW_WINDOW_NO_RESIZE, GL_TRUE);
   GLFWwindow window = glfwOpenWindow(config->window_width, config->window_height,
                                      GLFW_WINDOWED, config->window_title, NULL);
